9c20c1e8e2c60ccf55e0c2136413de96011c3803
MaxiLedesma93 <112991634+MaxiLedesma93@users.noreply.github.com>
2023-10-16 19:58:17 -0300

    formulario persona
    formulario persona

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.set('views', path.join(__dirname, '..', 'views'));
 app.set('view engine', 'pug');
@@ -16,19 +16,19 @@ router.get('/', function(req, res, next) {
 module.exports = router;
 
 /**************************/
-app.get('/', function(req, res, next) {
-  res.render('login', {
+app.get('/persona', function(req, res, next) {
+  res.render('formularioPersona', {
     pretty: true,
     user: { name: "ricardo", email: "gg@mail.com", city: "san luis" },
   });
 });
 /**************************/
-app.get('/login', function(req, res, next) {
+/* app.get('/login', function(req, res, next) {
   res.render('login', {
     pretty: true,
     user: { name: "ricardo", email: "gg@mail.com", city: "san luis" },
   });
-});
+}); */
 /**************************/
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/formularioPersona', function(req, res, next) {
+    res.send('formularioPersona.pug');
+});
+
 module.exports = router;
