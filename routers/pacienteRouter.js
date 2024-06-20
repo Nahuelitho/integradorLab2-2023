341aa931d4f543cba371ae86c18e1494d0186f50
MaxiLedesma93 <112991634+MaxiLedesma93@users.noreply.github.com>
2024-06-20 11:52:28 -0300

    Update pacienteRouter

--- routers/pacienteRouter.js
+++ routers/pacienteRouter.js
@@ -7,7 +7,7 @@ const Swal = require("sweetalert2");
 const pacienteController = require('../controllers/paciente.controller');
 //metodos de Paciente
 //lista todos los pacientes en una tabla
-//http:localhost/pacientes
+// http://localhost:3000/pacientes
 router.get("/", pacienteController.mostrarPacientes);
 // error al encontrar usuario o mail tambien intenta encontrar una persona con el dni ingresado. 
 ///crear, primero crea la persona y luego le asigna el id de esa persona al  Paciente (idPersona)
@@ -17,46 +17,9 @@ router.post("/crear", pacienteController.alta);
 //obtiene y muestra el paciente a actualizar
 router.get("/actualizar/:dni", pacienteController.mostrarPaciente);
 
-//persona/actualizar url ejecuta la actualizacion por DNI
-router.post("/actualizar/:dni", async (req, res, next) => { //el boton Guardar del Formulario Actualizar
-  const dni = req.params.dni;
-  const data = req.body;
-  const persona = await Persona.findOne({ where: { dni: dni } });
-  await Persona.update(
-    {
-      nombre: data.nombre,
-      apellido: data.apellido,
-      dni: data.dni,
-      telefono: data.telefono,
-      email: data.email,
-      fechaNacimiento: data.fechaNacimiento,
-      sexo: data.sexo,
-      domicilio: data.domicilio,
-      provincia: data.provincia,
-      localidad: data.localidad,
-      obraSocial: data.obraSocial,
-      numeroAfiliado: data.numeroAfiliado,
-      estado: true,
-      user: data.user,
-      password: data.password,
-    },
-    {
-      where: { dni: dni },
-    }
-  );
-  await Paciente.update(
-    {
-      embarazada: data.embar,
-      estado: true
-    },
-    {
-      where: { idPersona: persona.id },
-    }
-  );
+//pacientes/actualizar url ejecuta la actualizacion por DNI
+router.post("/actualizar/:dni", pacienteController.editarPaciente);
 
-  res.redirect("/paciente/pacientes");
-  next();
-});
 // hace borrado logico~ pasa el estado de true a false./
 router.post("/borrar/:dni", pacienteController.eliminarPaciente);
 
